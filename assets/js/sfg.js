c046e0059886e6859666f02a0acdc2e2986a6b05
Neo S <neoneosa@aol.com>
2017-06-21 19:13:59 +0200

    Many little things, most notably, however, is the new FAQs page.

--- assets/js/sfg.js
+++
@@ -1,5 +0,0 @@
-$('#senior_text').hover(function() {
-    document.getElementById('senior_text').setAttribute('style', 'color: #363636;')
-}, function() {
-    document.getElementById('senior_text').setAttribute('style', 'color: whitesmoke;')
-});
