6379e3f132e8d18a6f0dc83fbe935a735469bcb7
Neo S <neoneosa@aol.com>
2017-06-21 15:06:34 +0200

    Changed DEEZ COLORS

---
+++ assets/js/sfg.js
@@ -0,0 +1,5 @@
+$('#senior_text').hover(function() {
+    document.getElementById('senior_text').setAttribute('style', 'color: #363636;')
+}, function() {
+    document.getElementById('senior_text').setAttribute('style', 'color: whitesmoke;')
+});
